582b45f454fc74a09e52f055a04ce3a0e7923ef0
Toichubekov Bakdoolot <bakdoolottoychubekov@gmail.com>
2026-01-18 23:30:11 +0600

    third commit

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,4 +1,4 @@
-fetch('/assets/data/components.json')
+fetch('https://raw.githubusercontent.com/tcybkv/computers/refs/heads/main/assets/data/components.json')
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
